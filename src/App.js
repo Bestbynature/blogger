383860040561e3076e06c8b3279879bd99d758bd
Dammy <bestbynature2010@gmail.com>
2024-01-21 17:20:43 +0100

    Update the App dot js file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
+
+  const heading = 'The blog Heading'
+  const heading2 = 'The second heading'
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar plastic={heading} short={heading2} />
       <div className="content">
-        <Home />
+        <Home title={heading} />
       </div>
     </div>
   );
