241d0f014edda29b9651ecb4cb7e43ac2de95986
Dammy <bestbynature2010@gmail.com>
2024-01-22 17:19:33 +0100

    Add the navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,24 @@
 import { FaPlugCircleBolt } from "react-icons/fa6";
+import { FaSearchPlus } from "react-icons/fa";
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+  console.log('props in the navbare are as follows', props)
+
+  const plastic = props.plastic
+  const short = props.short
+
+
   return (
     <nav className="navbar">
-      <h1>Your Blog Plug <FaPlugCircleBolt className="plug"/></h1>
+      <h1 className="blog-plug">{plastic}<FaPlugCircleBolt className="plug"/></h1>
       <div className="links">
         <a href="/">Home</a>
         <a href="/create" style={{ 
           color: 'white', 
           backgroundColor: '#f1356d',
           borderRadius: '8px' 
-        }}>New Blog</a>
+        }}>New Blog <FaSearchPlus /></a>
       </div>
     </nav>
   );
