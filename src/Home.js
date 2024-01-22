9cd665cd238978947fc8199511d34ae86f345e78
Dammy <bestbynature2010@gmail.com>
2024-01-22 17:19:59 +0100

    Update the home component

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,32 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState();
+  const [blogs, setBlogs] = useState([
+    { id: 1, author: "Tobi", title: "My new website", body: "lorem ipsum..." },
+    {
+      id: 2,
+      author: "Emmanuel",
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+    },
+    {
+      id: 3,
+      author: "Ayooluwa",
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+    },
+  ]);
+
+
   return (
     <div className="home">
       <h2>Home Component</h2>
-      {/* {blogs.map((blog) => (
+      {blogs.map((blog, index) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
+          <h2>Title is {blog.title}</h2>
           <p>Written by {blog.author}</p>
         </div>
-      ))} */}
-
+      ))}
     </div>
   );
 };
@@ -19,15 +34,7 @@ const Home = () => {
 export default Home;
 
 
-// initial state of blogs
-
-/*
-[
-  { id: 1, author: "Tobi", title: "My new website", body: "lorem ipsum..." },
-  { id: 2, author: "Emmanuel", title: "Welcome party!", body: "lorem ipsum..." },
-  { id: 3, author: "Ayooluwa", title: "Web dev top tips", body: "lorem ipsum..." },
-
-]
-
-
-*/
+// Create a delete button for each blog post
+// create a function that will handle the delete button
+// create a bloglist component that will be used to display the list of blogs
+// introduce useEffect hook
